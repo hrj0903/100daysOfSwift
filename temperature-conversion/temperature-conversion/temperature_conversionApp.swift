3d8dfeaf52a24d2c5338fd79914c81652a90fad1
hrj0903 <heerij93@gmail.com>
2021-01-31 18:44:40 +0900

    add SwiftUI projects.

---
+++ temperature-conversion/temperature-conversion/temperature_conversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  temperature_conversionApp.swift
+//  temperature-conversion
+//
+//  Created by hrj on 2021/01/27.
+//
+
+import SwiftUI
+
+@main
+struct temperature_conversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
