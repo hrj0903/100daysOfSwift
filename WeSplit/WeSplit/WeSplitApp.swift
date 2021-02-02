7913a3035f6d9f306906ea09267de8f875ad6f44
hrj0903 <heerij93@gmail.com>
2021-02-02 22:22:17 +0900

    add SwiftUI projects.

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by hrj on 2021/01/26.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
